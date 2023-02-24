377d5dc7cb9e7f3977dfe6b4cbc86a830390184e
Eric Flumerfelt <eflumerf@fnal.gov>
2023-02-24 12:12:11 -0600

    Set default frame files as appropriate for each test. Labels are placeholders until assets are added

--- integtest/readout_type_scan.py
+++ integtest/readout_type_scan.py
@@ -73,6 +73,7 @@ except:
   conf_dict["boot"]["use_connectivity_service"] = False
   
 conf_dict["readout"]["data_rate_slowdown_factor"] = data_rate_slowdown_factor
+conf_dict["readout"]["default_data_file"] = "asset://label/ProtoWIB"
 
 swtpg_conf = copy.deepcopy(conf_dict)
 swtpg_conf["readout"]["enable_software_tpg"] = True
@@ -82,15 +83,23 @@ dqm_conf["dqm"]["enable_dqm"] = True
 
 wib2_conf = copy.deepcopy(conf_dict)
 wib2_conf["readout"]["clock_speed_hz"] = 62500000
+wib2_conf["readout"]["default_data_file"] = "asset://label/DuneWIB"
 
 pds_list_conf = copy.deepcopy(conf_dict)
 pds_list_conf["readout"]["hardware_map"] = integtest_file_gen.generate_hwmap_file(number_of_data_producers, 1, 2) # det_id = 2 for HD_PDS
+pds_list_conf["readout"]["default_data_file"] = "asset://label/PDSList"
 
 #tde_conf = copy.deepcopy(conf_dict)
 #tde_conf["readout"]["hardware_map"] = integtest_file_gen.generate_hwmap_file(number_of_data_producers, 1, 11) # det_id = 11 for VD_Top_TPC
+#tde_conf["readout"]["default_data_file"] = "asset://label/VDTPC"
 
 #pacman_conf = copy.deepcopy(conf_dict)
 #pacman_conf["readout"]["hardware_map"] = integtest_file_gen.generate_hwmap_file(number_of_data_producers, 1, 32) # det_id = 32 for NDLAr_TPC
+#pacman_conf["readout"]["default_data_file"] = "asset://label/PACMAN"
+
+#mpd_conf = copy.deepcopy(conf_dict)
+#mpd_conf["readout"]["hardware_map"] = integtest_file_gen.generate_hwmap_file(number_of_data_producers, 1, 33) # det_id = 33 for NDLAr_PDS
+#mpd_conf["readout"]["default_data_file"] = "asset://label/MPD"
 
 
 confgen_arguments={"WIB1_System": conf_dict,
@@ -99,7 +108,8 @@ confgen_arguments={"WIB1_System": conf_dict,
                    "WIB2_System": wib2_conf,
                    "PDS_(list)_System": pds_list_conf,
                    #"TDE_System": tde_conf,
-                   #"PACMAN_System": pacman_conf
+                   #"PACMAN_System": pacman_conf,
+                   #"MPD_System": mpd_conf
                   }
 
 # The commands to run in nanorc, as a list
