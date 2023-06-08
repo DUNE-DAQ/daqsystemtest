a5eaaa9de6e8acdd472df6c6c3e9ba990fc4e311
Alessandro Thea <alessandro.thea@cern.ch>
2023-06-08 11:52:56 +0200

    APA1 woking again

--- test/test_k8s_tests.py
+++ test/test_k8s_tests.py
@@ -25,26 +25,27 @@ flx_commands = [
 ]
 
 daq_commands = [
-    f"daqconf_multiru_gen {' '.join(common_flags)} --force-pm k8s -c {cfg_dir}/np04_daq.json -m {cfg_dir}/np04_APA1_DetReadoutMap.json --debug daq_APA1_k8s",
-    f"daqconf_multiru_gen {' '.join(common_flags)} --force-pm ssh -c {cfg_dir}/np04_daq.json -m {cfg_dir}/np04_APA1_DetReadoutMap.json --debug daq_APA1_ssh",
+    f"daqconf_multiru_gen {' '.join(common_flags)} --force-pm k8s -c {cfg_dir}/np04_daq_tp.json -m {cfg_dir}/np04_APA1_DetReadoutMap.json --debug daq_APA1_k8s",
+    f"daqconf_multiru_gen {' '.join(common_flags)} --force-pm ssh -c {cfg_dir}/np04_daq_tp.json -m {cfg_dir}/np04_APA1_DetReadoutMap.json --debug daq_APA1_ssh",
 ]
 
 commands = daq_commands + flx_commands + wib_commands
+commands = daq_commands 
 
 
 failed = []
 success = []
-# for cmd in commands:
-#     print(f"Executing '{cmd}'")
-#     cmd_tokens = cmd.split()
-#     exe = getattr(sh, cmd_tokens[0])
-#     try:
-#         exe(*cmd_tokens[1:], _out=sys.stdout, _err=sys.stderr)
-#     except sh.ErrorReturnCode:
-#         failed.append(cmd)
-#         continue
+for cmd in commands:
+    print(f"Executing '{cmd}'")
+    cmd_tokens = cmd.split()
+    exe = getattr(sh, cmd_tokens[0])
+    try:
+        exe(*cmd_tokens[1:], _out=sys.stdout, _err=sys.stderr,  _new_session=False)
+    except sh.ErrorReturnCode:
+        failed.append(cmd)
+        continue
 
-#     success.append(cmd)
+    success.append(cmd)
 
 
 print(f"Successful {success}")
