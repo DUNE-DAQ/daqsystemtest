fd188d23f25f6a2ed5fd472ceab2d0d79648bd49
Giovanna Lehmann Miotto <glehmann@np04-srv-024.cern.ch>
2023-07-11 19:27:10 +0200

    use a single config generation file for all large and medium scale tests

--- test/test_scale_tests.py
+++ test/test_scale_tests.py
@@ -11,10 +11,9 @@ cfg_dir = script_path.parent / "config" / "scale_tests"
 
 commands = [
     f"daqconf_multiru_gen -n --detector-readout-map-file {cfg_dir}/large_scale_system_DetReadoutMap.json -c {cfg_dir}/large_scale_system.json test",
-    f"daqconf_multiru_gen -n --detector-readout-map-file {cfg_dir}/large_scale_system_DetReadoutMap.json -c {cfg_dir}/large_scale_system_k8s.json test",
-#    f"daqconf_multiru_gen -n --detector-readout-map-file {cfg_dir}/large_scale_system_DetReadoutMap.json -c {cfg_dir}/large_scale_3DF.json test",
-    f"daqconf_multiru_gen -n --detector-readout-map-file {cfg_dir}/medium_scale_system_DetReadoutMap.json -c {cfg_dir}/medium_scale_system.json test",
-    f"daqconf_multiru_gen -n --detector-readout-map-file {cfg_dir}/medium_scale_system_DetReadoutMap.json -c {cfg_dir}/medium_scale_system_k8s.json test",
+    f"daqconf_multiru_gen -n --force-pm k8s --detector-readout-map-file {cfg_dir}/large_scale_system_DetReadoutMap.json -c {cfg_dir}/large_scale_system.json test",
+    f"daqconf_multiru_gen -n --detector-readout-map-file {cfg_dir}/medium_scale_system_DetReadoutMap.json -c {cfg_dir}/large_scale_system.json test",
+    f"daqconf_multiru_gen -n --force-pm k8s --detector-readout-map-file {cfg_dir}/medium_scale_system_DetReadoutMap.json -c {cfg_dir}/large_scale_system.json test",
 ]
 
 failed = []
