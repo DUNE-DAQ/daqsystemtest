d10b58b2eac4d192d64b4e25cf230db7b7fcaa98
Kurt Biery <biery@daq.fnal.gov>
2023-12-14 11:39:05 -0600

    Tweaked tpset_min_latency values in integtests to reduce the chances of warnings

--- integtest/3ru_1df_multirun_test.py
+++ integtest/3ru_1df_multirun_test.py
@@ -116,7 +116,7 @@ swtpg_conf["readout"]["enable_tpg"] = True
 swtpg_conf["readout"]["tpg_threshold"] = 500
 swtpg_conf["readout"]["tpg_algorithm"] = "SimpleThreshold"
 swtpg_conf["readout"]["default_data_file"] = "asset://?checksum=dd156b4895f1b06a06b6ff38e37bd798" # WIBEth All Zeros
-swtpg_conf["readout"]["tpset_min_latency_ticks"] = 6250000
+swtpg_conf["readout"]["tpset_min_latency_ticks"] = 9375000
 swtpg_conf["trigger"]["trigger_activity_plugin"] = ["TriggerActivityMakerPrescalePlugin"]
 swtpg_conf["trigger"]["trigger_activity_config"] = [ {"prescale": 25} ]
 swtpg_conf["trigger"]["trigger_candidate_plugin"] = ["TriggerCandidateMakerPrescalePlugin"]
