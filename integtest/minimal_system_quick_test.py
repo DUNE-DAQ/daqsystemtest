042428c41282d9b381c2a4629befccef224a49fc
Andrew Mogan <amogan@fnal.gov>
2024-08-28 10:17:17 -0500

    Add factors of 100 so the tests will fail

--- integtest/minimal_system_quick_test.py
+++ integtest/minimal_system_quick_test.py
@@ -21,27 +21,27 @@ expected_event_count_tolerance=2
 wib1_frag_hsi_trig_params={"fragment_type_description": "WIB",
                            "fragment_type": "ProtoWIB",
                            "hdf5_source_subsystem": "Detector_Readout",
-                           "expected_fragment_count": number_of_data_producers,
+                           "expected_fragment_count": 100*number_of_data_producers,
                            "min_size_bytes": 37656, "max_size_bytes": 37656}
 wib2_frag_params={"fragment_type_description": "WIB2",
                   "fragment_type": "WIB",
                   "hdf5_source_subsystem": "Detector_Readout",
-                  "expected_fragment_count": number_of_data_producers,
+                  "expected_fragment_count": 100*number_of_data_producers,
                   "min_size_bytes": 29808, "max_size_bytes": 30280}
 wibeth_frag_params={"fragment_type_description": "WIBEth",
                   "fragment_type": "WIBEth",
                   "hdf5_source_subsystem": "Detector_Readout",
-                  "expected_fragment_count": number_of_data_producers,
+                  "expected_fragment_count": 100*number_of_data_producers,
                   "min_size_bytes": 7272, "max_size_bytes": 14472}
 triggercandidate_frag_params={"fragment_type_description": "Trigger Candidate",
                               "fragment_type": "Trigger_Candidate",
                               "hdf5_source_subsystem": "Trigger",
-                              "expected_fragment_count": 1,
+                              "expected_fragment_count": 100*1,
                               "min_size_bytes": 72, "max_size_bytes": 216}
 hsi_frag_params ={"fragment_type_description": "HSI",
                              "fragment_type": "Hardware_Signal",
                              "hdf5_source_subsystem": "HW_Signals_Interface",
-                             "expected_fragment_count": 1,
+                             "expected_fragment_count": 100*1,
                              "min_size_bytes": 72, "max_size_bytes": 100}
 ignored_logfile_problems={"connectionservice": ["Searching for connections matching uid_regex<errored_frames_q> and data_type Unknown"]}
 
@@ -88,7 +88,7 @@ def test_log_files(run_nanorc):
 
 def test_data_files(run_nanorc):
     # Run some tests on the output data file
-    assert len(run_nanorc.data_files)==expected_number_of_data_files
+    assert len(run_nanorc.data_files)==100*expected_number_of_data_files
 
     fragment_check_list=[triggercandidate_frag_params, hsi_frag_params]
     #fragment_check_list.append(wib1_frag_hsi_trig_params) # ProtoWIB
