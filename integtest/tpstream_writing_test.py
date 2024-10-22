9e8b3953407e729ddaa8da70a39acd745dff922f
Kurt Biery <biery@daq.fnal.gov>
2024-10-22 15:37:27 -0500

    Increased the min_size_bytes for WIBEth fragments from 72 to 7272 in several integtests since we believe that there should always be at least one frame retrieved per data request no matter how small the readout window is.

--- integtest/tpstream_writing_test.py
+++ integtest/tpstream_writing_test.py
@@ -38,7 +38,7 @@ wibeth_frag_multi_trig_params = {
     "fragment_type": "WIBEth",
     "hdf5_source_subsystem": "Detector_Readout",
     "expected_fragment_count": (number_of_data_producers * number_of_readout_apps),
-    "min_size_bytes": 72,
+    "min_size_bytes": 7272,
     "max_size_bytes": 14472,
 }
 wibeth_tpset_params = {
