26770db74fe5a9463d48d5ae6c4b795866aa522b
Kurt Biery <biery@daq.fnal.gov>
2026-05-21 14:18:53 -0500

    Updated the new cleanup test in sample_ehn1_multihost_test.py to check for conditions that cause the integtest to be skipped.

--- integtest/sample_ehn1_multihost_test.py
+++ integtest/sample_ehn1_multihost_test.py
@@ -458,4 +458,9 @@ def test_tpstream_files(run_dunerc):
 
 
 def test_cleanup(run_dunerc):
+    if len(computers_that_are_unreachable) > 0:
+        pytest.skip(f"One or more needed computers are unreachable ({computers_that_are_unreachable}).")
+    if not pytest_tmpdir_looks_reasonable:
+        pytest.skip("The PYTEST_DEBUG_TEMPROOT env var has not been set to point to a valid directory.")
+
     utility_functions.remove_hdf5_files_if_requested(run_dunerc, this_test_requests_hdf5_file_removal=False)
