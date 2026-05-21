810027227994bddcfb686e16eed90a9b919071fd
Kurt Biery <biery@daq.fnal.gov>
2026-05-21 10:08:17 -0500

    Added the disabled_tpg_test by merging from the develop branch and modified it to include the HDF5 file removal changes.

--- integtest/disabled_tpg_test.py
+++ integtest/disabled_tpg_test.py
@@ -14,9 +14,9 @@ import os
 
 import integrationtest.data_file_checks as data_file_checks
 import integrationtest.log_file_checks as log_file_checks
-import integrationtest.basic_checks as basic_checks
 import integrationtest.data_classes as data_classes
 import integrationtest.resource_validation as resource_validation
+import integrationtest.utility_functions as utility_functions
 from integrationtest.get_pytest_tmpdir import get_pytest_tmpdir
 from integrationtest.verbosity_helper import IntegtestVerbosityLevels
 
@@ -132,7 +132,7 @@ dunerc_command_list = (
 
 def test_dunerc_success(run_dunerc, caplog):
     # check for run control success, problems during pytest setup, etc.
-    basic_checks.basic_checks(run_dunerc, caplog, print_test_name=True)
+    utility_functions.basic_checks(run_dunerc, caplog, print_test_name=True)
 
 
 def test_log_files(run_dunerc):
@@ -186,3 +186,7 @@ def test_data_files(run_dunerc):
             )
 
     assert all_ok
+
+
+def test_cleanup(run_dunerc):
+    utility_functions.remove_hdf5_files_if_requested(run_dunerc, this_test_requests_hdf5_file_removal=False)
